6fbaf67766be40935d98a0d449f32a3a44048ccc
Philippe Beck <philippe@philippebeck.net>
2018-01-23 07:49:50 +0100

    Adding the datas.js

---
+++ public/js/datas.js
@@ -0,0 +1,145 @@
+// ***************** \\
+// ***** DATAS ***** \\
+// ***************** \\
+
+
+
+// You need to put this file in your js public folder, then call it from your layout before jim.js
+// And you need to put actions for your project in each function
+// For example, I put some links in some of them
+
+
+
+// Strict mode
+'use strict';
+
+
+// Function for A action
+function keyA() {
+  // You can put an action here !
+}
+
+// Function for B action
+function keyB() {
+  // You can put an action here !
+}
+
+// Function for C action
+function keyC() {
+  location = 'https://codepen.io';
+}
+
+// Function for D action
+function keyD() {
+  // You can put an action here !
+}
+
+// Function for E action
+function keyE() {
+  // You can put an action here !
+}
+
+// Function for F action
+function keyF() {
+  location = 'https://www.freecodecamp.com';
+}
+
+// Function for G action
+function keyG() {
+  location = 'https://github.com';
+}
+
+// Function for H action
+function keyH() {
+  location = './';
+}
+
+// Function for I action
+function keyI() {
+  location = 'https://www.instagram.com/?hl=fr';
+}
+
+// Function for J action
+function keyJ() {
+  document.getElementById('jim-help').classList.toggle('none');
+}
+
+// Function for K action
+function keyK() {
+  // You can put an action here !
+}
+
+// Function for L action
+function keyL() {
+  location = 'https://www.linkedin.com';
+}
+
+// Function for M action
+function keyM() {
+  location = 'https://medium.com';
+}
+
+// Function for N action
+function keyN() {
+  location = 'https://www.npmjs.com';
+}
+
+// Function for O action
+function keyO() {
+  location = 'https://openclassrooms.com';
+}
+
+// Function for P action
+function keyP() {
+  location = 'https://packagist.org';
+}
+
+// Function for Q action
+function keyQ() {
+  // You can put an action here !
+}
+
+// Function for R action
+function keyR() {
+  // You can put an action here !
+}
+
+// Function for S action
+function keyS() {
+  location = 'https://stackoverflow.com';
+}
+
+// Function for T action
+function keyT() {
+  location = 'https://twitter.com';
+}
+
+// Function for U action
+function keyU() {
+  // You can put an action here !
+}
+
+// Function for V action
+function keyV() {
+  // You can put an action here !
+}
+
+// Function for W action
+function keyW() {
+  location = 'https://3wa.fr';
+}
+
+// Function for X action
+function keyX() {
+  // You can put an action here !
+}
+
+// Function for Y action
+function keyY() {
+  location = 'https://www.youtube.com';
+}
+
+// Function for Z action
+function keyZ() {
+  // You can put an action here !
+}
