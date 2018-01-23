1f178c5b881c2453e414f2a96945ef30957d445f
Philippe Beck <philippe@philippebeck.net>
2018-01-23 07:23:15 +0100

    Added public folder skeleton & front-end master files

---
+++ public/js/script.js
@@ -0,0 +1,17 @@
+// *********************** \\
+// ***** MAIN SCRIPT ***** \\
+// *********************** \\
+
+
+
+// Strict mode
+'use strict';
+
+
+
+// Waits the complete page loading to launch the scripts
+document.addEventListener('DOMContentLoaded', function(e)
+{
+  // Put your project script here
+  }
+});
